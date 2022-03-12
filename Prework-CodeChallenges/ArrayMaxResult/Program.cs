2de07b1b37e4c725393e341e38c15871fec3dc34
olashlool <olashlool94@gmail.com>
2022-03-12 06:37:32 +0200

    Solved Problem 1

---
+++ Prework-CodeChallenges/ArrayMaxResult/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreworkCodeChallenges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            challenge1();
+        }
+
+        static void challenge1()
+        {
+            Console.Write("Enter 5 Number btween 1-10 separate by space :");
+            string userInput = Console.ReadLine();
+            string[] strArr = userInput.Split();
+
+            Console.Write("Please Select Number between [" + string.Join(", ", strArr) + "] ");
+            string slectNumber = Console.ReadLine();
+            int count = 0;
+            foreach (string i in strArr)
+            {
+                if (i == slectNumber)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"The number {slectNumber} has a score of {int.Parse(slectNumber) * count}");
+        }
+
+
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Prework-CodeChallenges
+
+## Array Max Result
+
+[Code](Prework-CodeChallenges/ArrayMaxResult/Program.cs)
+
+#### Output :
+![OutputConsole](img/screenshot1.png)
+
+<hr>
+<hr>
